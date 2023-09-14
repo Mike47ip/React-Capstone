b0cf25777eaa1ebdb71d8e47773ddea471274853
Mike Dark <agyemankojo47@gmail.com>
2023-09-14 21:24:31 +0000

    Create try err slice branch

--- src/redux/features/selectedMinionSlice.js
+++ src/redux/features/selectedMinionSlice.js
@@ -1,22 +1,22 @@
-// // selectedMinionSlice.js
+// selectedMinionSlice.js
 
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-// const selectedMinionSlice = createSlice({
-//   name: 'selectedMinion',
-//   initialState: {
-//     selectedMinion: null,
-//   },
-//   reducers: {
-//     setSelectedMinion: (state, action) => {
-//       state.selectedMinion = action.payload;
-//     },
-//     clearSelectedMinion: (state) => {
-//       state.selectedMinion = null;
-//     },
-//   },
-// });
+const selectedMinionSlice = createSlice({
+  name: 'selectedMinion',
+  initialState: {
+    selectedMinion: null,
+  },
+  reducers: {
+    setSelectedMinion: (state, action) => {
+      state.selectedMinion = action.payload;
+    },
+    clearSelectedMinion: (state) => {
+      state.selectedMinion = null;
+    },
+  },
+});
 
-// export const { setSelectedMinion, clearSelectedMinion } = selectedMinionSlice.actions;
+export const { setSelectedMinion, clearSelectedMinion } = selectedMinionSlice.actions;
 
-// export default selectedMinionSlice.reducer;
+export default selectedMinionSlice.reducer;
